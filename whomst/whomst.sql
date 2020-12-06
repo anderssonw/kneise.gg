8bbee1a5e57db968e8c75aad77d3ebd58c905db1
Thomas Aven <avensin@hotmail.com>
2020-12-06 08:36:21 +0000

    sql

--- whomst/whomst.sql
+++ whomst/whomst.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS whomst(
        display_name VARCHAR NOT NULL,
        connect_code VARCHAR NOT NULL,
        ip_address VARCHAR NOT NULL,
-       region VARCHAR NOT NULL
+       region VARCHAR NOT NULL,
+       UNIQUE(display_name, connect_code, ip_address)
 );
